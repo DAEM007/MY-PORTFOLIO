9e81a84601be0e9c6a91c92b02c9edecb7e13123
EMMANUEL <dammylola299@gmail.com>
2022-05-22 02:55:50 +0100

    worked on carousel & started contact section

--- script.js
+++ script.js
@@ -8,10 +8,32 @@ $(document).ready(function(){
         }
     });
 
-    // MENU BAR TOGGLE STYLES
+    // MENU BAR TOGGLE SCRIPT
     $('.menu-btn').click(function(){
         $('.nav-bar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
         $('.nav-bar.sticky .menu li a:hover').toggleClass('color', 'purple')
+    });
+
+    // OWL CAROUSEL SCRIPT
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false,
+            },
+            600:{
+                items: 2,
+                nav: false,
+            },
+            1000:{
+                items: 3,
+                nav: false,
+            }
+        }
     });
 });
